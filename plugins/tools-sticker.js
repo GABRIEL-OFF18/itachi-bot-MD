7140510e65d31aa2da9c7393a18b77e56a91ab10
GABRIEL-OFF18 <virginiarimachipalmares@gmail.com>
2026-02-05 16:24:30 -0500

    Actualizar tools-sticker.js

--- plugins/tools-sticker.js
+++ plugins/tools-sticker.js
@@ -101,7 +101,7 @@ conn.sendMessage(m.chat, { text: `⚠︎ Se ha producido un problema.\n> Usa *${
 }}
 
 handler.tags = ['sticker']
-handler.help = ['brat', 'bratv', 'emojimix', 'qc', 'take', 'robar', 'wm']
+handler.help = ['brat', 'bratv', 'emojimix', 'take', 'robar', 'wm']
 handler.command = ['brat', 'bratv', 'emojimix', 'qc', 'take', 'wm']
 
 export default handler
